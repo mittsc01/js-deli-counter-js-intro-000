aa7280c9e650e266baaa61d539481618cf22428e
Scott Mittman <sm2884@nau.edu>
2020-06-11 21:49:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(currentLine,person){
   currentLine.push(person)
   return `Welcome, ${person}. You are number ${currentLine.length} in line.`
 }
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length>=1){
+    return katzDeliLine.shift()
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
